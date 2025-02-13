e845a45e9798d4fd6eda694ed0ecf71ad0a35bf9
lxthxrgx <osu.bonse@gmail.com>
2025-02-13 09:20:03 +0200

    Update 7.1:///

---
+++ ACG-Class/Model/NewClass/D2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACG_Class.Model.NewClass
+{
+    interface I2D
+    {
+        int Id { get; set; }
+        int NumberGroup { get; set; }
+        string NameGroup { get; set; }
+    }
+    interface I2DOtherInfo
+    {
+        string? Pibs { get; set; }
+        string Address { get; set; }
+        double? Area { get; set; }
+        bool? isAlert { get; set; }
+        DateTime? DateCloseDepartment { get; set; }
+    }
+
+
+    public class _2D : I2D, I2DOtherInfo
+    {
+        public int Id { get; set; }
+        public int NumberGroup { get; set; }
+        public string NameGroup { get; set; }
+
+        public string? Pibs { get; set; }
+        public string Address { get; set; }
+        public double? Area { get; set; }
+        public bool? isAlert { get; set; } = false;
+        public DateTime? DateCloseDepartment { get; set; }
+    }
+}
