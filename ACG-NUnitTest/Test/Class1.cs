2a17db2ea97f7670bc5876291432b6e621477ad9
lxthxrgx <osu.bonse@gmail.com>
2025-06-19 16:05:55 +0300

    add Jenkinsfile

--- ACG-NUnitTest/Test/Class1.cs
+++ ACG-NUnitTest/Test/Class1.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace ACG_NUnitTest.Test
-{
-    public class TestDdeleteSpace
-    {
-        [TestCase(" UA1830529900005638792043\r\nАТ КБ \"ПриватБанк\"")]
-        public void GetClearString(string Test)
-        {
-            var result = DeleteSpace.Deletespace(Test);
-            Console.WriteLine(result);
-            Assert.That(result, Is.EqualTo("UA183052990000648935204043 АТ КБ \"ПриватБанк\""));
-        }
-    }    
-}
+// namespace ACG_NUnitTest.Test
+// {
+//     public class TestDdeleteSpace
+//     {
+//         [TestCase(" UA1830529900005638792043\r\nАТ КБ \"ПриватБанк\"")]
+//         public void GetClearString(string Test)
+//         {
+//             var result = DeleteSpace.Deletespace(Test);
+//             Console.WriteLine(result);
+//             Assert.That(result, Is.EqualTo("UA183052990000648935204043 АТ КБ \"ПриватБанк\""));
+//         }
+//     }    
+// }
