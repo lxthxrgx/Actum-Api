b75ea55b61ac343eae138f217e781f31458233b8
lxthxrgx <osu.bonse@gmail.com>
2025-06-30 16:07:22 +0300

    fix bugs

--- ACG-Api/service/AutoDocService/SubleaseFopDogAct.cs
+++ ACG-Api/service/AutoDocService/SubleaseFopDogAct.cs
@@ -11,7 +11,7 @@ namespace ACG_Api.service.AutoDocService
     {
         public void SubleaseFopDogActCreate(DTOSubleaseFopDogAct subleaseData)
         {
-            XPath xpathSublease = new XPath("/home/ltx/Documents/Sublease.docx");
+            XPath xpathSublease = new XPath("/home/ltx/Documents/Sublease-Fop/SubleaseDogFop.docx");
             xpathSublease.WriteXmlTree("ContractNumber", subleaseData.ContractNumber);
             xpathSublease.WriteXmlTree("CreationDate", subleaseData.CreationDate.ToString("dd.MM.yyyy"));
             xpathSublease.WriteXmlTree("PipSublessor", subleaseData.PipSublessor);
@@ -26,19 +26,20 @@ namespace ACG_Api.service.AutoDocService
             xpathSublease.WriteXmlTree("subleaseDopStartDate", subleaseData.subleaseDopStartDate.ToString("dd.MM.yyyy"));
             xpathSublease.WriteXmlTree("subleaseDopName", subleaseData.subleaseDopName);
             xpathSublease.WriteXmlTree("subleaseDopRnokpp", subleaseData.subleaseDopRnokpp);
-            xpathSublease.WriteXmlTree("StrokDii", subleaseData.EndContractData.ToString("dd.MM.yyyy"));
+            xpathSublease.WriteXmlTree("EndContractData", subleaseData.EndContractData.ToString("dd.MM.yyyy"));
             xpathSublease.WriteXmlTree("Pricing", subleaseData.Pricing.ToString());
             xpathSublease.WriteXmlTree("PricingText", subleaseData.PricingText);
             xpathSublease.WriteXmlTree("BanckAccount", subleaseData.BanckAccount);
             
             xpathSublease.Save($"{subleaseData.NumberGroup} - {subleaseData.NameGroup} - договір");
 
-            XPath xpathSublease2 = new XPath("/home/ltx/Documents/Sublease2.docx");
+            XPath xpathSublease2 = new XPath("/home/ltx/Documents/Sublease-Fop/SubleaseActFop.docx");
             xpathSublease2.WriteXmlTree("CreationDate", subleaseData.CreationDate.ToString("dd.MM.yyyy"));
+            xpathSublease2.WriteXmlTree("ContractNumber", subleaseData.ContractNumber);
             xpathSublease2.WriteXmlTree("PipSublessor", subleaseData.PipSublessor);
             xpathSublease2.WriteXmlTree("PipsSublessor", subleaseData.PipsSublessor);
             xpathSublease2.WriteXmlTree("rnokppSublessor", subleaseData.rnokppSublessor);
-            xpathSublease.WriteXmlTree("Edruofop", subleaseData.Edruofop);
+            xpathSublease2.WriteXmlTree("Edruofop", subleaseData.Edruofop);
             xpathSublease2.WriteXmlTree("addressSublessor", subleaseData.addressSublessor);
 
             xpathSublease2.WriteXmlTree("subleaseDopContractNumber", subleaseData.subleaseDopContractNumber);
